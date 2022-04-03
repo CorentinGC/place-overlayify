f0066fccf671af50a70658665962265ea7f1e15b
Ali Furkan Yıldız <alifurkanyildiz@gmail.com>
2022-04-04 01:16:00 +0300

    color palette fix

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const Jimp = require("jimp");
 const program_mode = true;
 
 const palette = [
-	0xbe0039, 0xff4500, 0xffa800, 0xffd635, 0x00a368, 0x00cc78, 0x7eed56,
-	0x00756f, 0x009eaa, 0x2450a4, 0x3690ea, 0x51e9f4, 0x493ac1, 0x6a5cff,
-	0x811e9f, 0xb44ac0, 0xff3881, 0xff99aa, 0x6d482f, 0x9c6926, 0x000000,
-	0x898d90, 0xd4d7d9, 0xffffff
+	7143450, 12451897, 16729344, 16754688, 16766517, 16775352, 41832, 52344,
+	8318294, 30063, 40618, 52416, 2379940, 3576042, 5368308, 4799169, 6970623,
+	9745407, 8461983, 11815616, 14986239, 14553215, 16726145, 16751018, 7161903,
+	10250534, 16757872, 0, 5329490, 9014672, 13948889, 16777215
 ].map((c) => c * 256 + 255);
+
+// #rrggbbaa
+const red = (x) => (x & 0xff000000) >>> 24;
+const green = (x) => (x & 0x00ff0000) >>> 16;
+const blue = (x) => (x & 0x0000ff00) >>> 8;
+const alpha = (x) => x & 0x000000ff;
+const rgba = (x) => [red(x), green(x), blue(x), alpha(x)];
+
+function difference(c1, c2) {
+	let diff = 0;
+	for (let i = 0; i < c1.length; i++) diff += (c1[i] - c2[i]) * (c1[i] - c2[i]);
+	return Math.sqrt(diff);
+}
 
 function overlayify(images) {
 	return new Promise(async (resolve, reject) => {
@@ -24,8 +37,12 @@ function overlayify(images) {
 					canvas.setPixelColor(col - (col % 0x100), px, py);
 				} else if (col !== 0x00000000) {
 					// replace with closest color in the palette
+					// check red green blue values
 					canvas.setPixelColor(
-						palette.sort((a, b) => Math.abs(a - col) - Math.abs(b - col))[0],
+						palette.sort(
+							(a, b) =>
+								difference(rgba(col), rgba(a)) - difference(rgba(col), rgba(b))
+						)[0],
 						px,
 						py
 					);
